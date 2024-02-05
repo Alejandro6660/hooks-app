6712aededbfbf1eb706bbb87c31f47c27f27a7b4
Alejandro <alejandro.fuentesgzm@gmail.com>
2024-02-05 08:33:35 -0600

    useReducer

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 /* import { HooksApp } from "./HooksApp.jsx"; */
 /* import { CounterApp } from "./01-useState/CounterApp.jsx"; */
-// import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook.jsx";
+//import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook.jsx";
 // import { SimpleForm } from "./02-useEffect/SimpleForm.jsx";
 // import { SimpleFormCustomHook } from "./02-useEffect/SimpleFormCustomHook.jsx";
 // import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks.jsx";
 // import { FocusScreen } from "./04-useRef/FocusScreen.jsx";
+//?import "./07-useReducer/intro-reducer.js";
 import { Memorize } from "./05-memo/Memorize.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Memorize />
+    <Memorize></Memorize>
   </React.StrictMode>
 );
